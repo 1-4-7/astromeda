afe2126b3e3188f798ab9fa7eb40f0222592ad3f
emin_bzdg_147 <64196760+1-4-7@users.noreply.github.com>
2021-05-30 13:49:57 +0300

    .

---
+++ discord-yashinu-modbot-master/Events/ready.js
@@ -0,0 +1,21 @@
+
+const qdb = require("quick.db");
+const db = new qdb.table("ayarlar");
+const ayar = db.get('ayar') || {};
+const ayarlar = require('../ayarlar.json');
+const client = global.client;
+module.exports = () => {
+  console.log ('_________________________________________');
+  console.log (`Kullanıcı İsmi     : ${client.user.username}`);
+  console.log (`Sunucular          : ${client.guilds.cache.size}`);
+  console.log (`Kullanıcılar       : ${client.users.cache.size}`);
+  console.log (`Prefix             : ${ayarlar.prefix}`);
+  console.log (`Durum              : Bot Çevrimiçi!`);
+  console.log ('_________________________________________');
+  client.user.setActivity("Eon147 ❤️ Eon");
+  if (ayar.botSesKanali && client.channels.cache.has(ayar.botSesKanali)) client.channels.cache.get(ayar.botSesKanali).join().catch();
+}
+module.exports.configuration = {
+  name: "ready"
+  
+}
--- stat-astromeda/bot.js
+++
@@ -1,7 +0,0 @@
-const client = global.client;
-
-client.on("ready", () => {
-    console.log("Let's goo!");
-});
-
-client.login(global.Settings.Token);
